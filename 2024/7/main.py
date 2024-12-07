89b447fdf7daab8a2cd906c41634d23f22f4e5af
Ivo Schols <IvoSchols97@gmail.com>
2024-12-07 15:34:02 +0100

    Refactor Day 7

--- 2024/7/main.py
+++ 2024/7/main.py
@@ -1,101 +1,51 @@
 import itertools
 
 
-def part_one(equations):
-    
-    operators = ['+','*']
+def solve_equations(equations, operators):
     total = 0
 
-    # Test combinations of operators and numbers to make the equation true
     for equation in equations:
         left_handside = equation[0]
         right_handside = equation[1:]
 
-        # Get cartesian product of operators and numbers (remember an operator is always between two numbers)
-        possible_combinations = itertools.product(operators, repeat=len(right_handside)-1)
+        possible_combinations = itertools.product(operators, repeat=len(right_handside) - 1)
 
         for combination in possible_combinations:
-            sum = right_handside[0]
-            # Apply the combination to the right handside
+            result = right_handside[0]
+
             for i, operator in enumerate(combination):
                 index = i + 1
 
                 if operator == '+':
-                    sum += right_handside[index]
+                    result += right_handside[index]
                 elif operator == '*':
-                    sum *= right_handside[index]
-            # Check if the sum is equal to the left handside
-            if sum == left_handside:
-                total += sum
-                break
-
-
-
-    print(total)
-        
-
-            
-def part_two(equations):
-
-    operators = ['+','*', '||']
-    total = 0
-
-    # Test combinations of operators and numbers to make the equation true
-    for equation in equations:
-
-        left_handside = equation[0]
-        right_handside = equation[1:]
-
-        # Get cartesian product of operators and numbers (remember an operator is always between two numbers)
-        possible_combinations = itertools.product(operators, repeat=len(right_handside)-1)
-
-        for combination in possible_combinations:
-            sum = right_handside[0]
-            # Apply the combination to the right handside
-            for i, operator in enumerate(combination):
-                index = i + 1
-
-                if operator == '+':
-                    sum += right_handside[index]
-                elif operator == '*':
-                    sum *= right_handside[index]
+                    result *= right_handside[index]
                 elif operator == '||':
-                    sum = int(str(sum) + str(right_handside[index]))
-            # Check if the sum is equal to the left handside
-            if sum == left_handside:
-                total += sum
+                    result = int(str(result) + str(right_handside[index]))
+
+            if result == left_handside:
+                total += result
                 break
 
-    print(total)
+    return total
 
 
 def main():
-    input_file = open('./7/input.txt', 'r')
-    
-    # Read the input file
-    lines = input_file.readlines()
-    input_file.close()
+    with open('./7/input.txt', 'r') as input_file:
+        lines = input_file.readlines()
 
-    # Build the equations to solve
-    equations = []
+    equations = [
+        [int(parts[0])] + list(map(int, parts[1].strip().split(' ')))
+        for line in lines
+        for parts in [line.split(':')]
+    ]
 
-    # Split on :
-    for line in lines:
-        equation = []
-        parts = line.split(':')
-        left_handside = int(parts[0])
-        equation.append(left_handside)
-        # Strip and cast to int the right handside
-        right_handside = parts[1].strip().split(' ')
-        right_handside = [int(x) for x in right_handside]
-        equation.extend(right_handside)
-        equations.append(equation)
-        
+    part_one = solve_equations(equations, operators=['+', '*'])
+    part_two = solve_equations(equations, operators=['+', '*', '||'])
 
+    print(part_one)
+    print(part_two)
 
-
-    part_one(equations)
-    part_two(equations)
 
 if __name__ == '__main__':
     main()
